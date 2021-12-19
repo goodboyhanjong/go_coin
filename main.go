510c7a8f153097a9014477a4702a7e2fceccf0c4
hanjonghun <jh.han@muzlive.com>
2021-12-19 16:22:09 +0900

    printf에 대한 이해 - formatting

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("It Works!")
-	var nico string = "nico"
-	nico2 := "nico"
+	// var nico string = "nico"
+	// nico2 := "nico"
+	test_var := 128
+	fmt.Printf("%b\n", test_var)
 }
